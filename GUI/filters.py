5c7bac01dcbbdd945c8d939700484c20a0c7ae64
Riddimental <juan.villota@correounivalle.edu.co>
2024-03-07 22:52:44 +0000

    implemented segmentation methods as thershold, isodata y region growing

--- GUI/filters.py
+++ GUI/filters.py
@@ -1,21 +1,240 @@
 import math
+import sys
+import time
 import nibabel as nib
 import numpy as np
+#np.set_printoptions(threshold=sys.maxsize)
 import matplotlib.pyplot as plt
+from PIL import Image, ImageTk, ImageDraw
 import cv2
 
 def gaussian(data, intensity):
    data = cv2.GaussianBlur(data,(math.trunc(intensity),math.trunc(intensity)),0)
+   #print("data",data)
    plt.imshow(data, cmap="gray")
    plt.xticks([])
    plt.yticks([])
-   plt.savefig("output/plot.png", format='png', dpi= 120 , bbox_inches='tight', pad_inches=0)
-   print("gaussian applied")
+   time.sleep(0.0016)
+   plt.savefig("temp/plot.png", format='png', dpi= 120 , bbox_inches='tight', pad_inches=0)
+   #print("gaussian applied")
    
-def laplacian(data):
-   data = cv2.Laplacian(data, -1, ksize=5, scale=1,delta=0, borderType=cv2.BORDER_DEFAULT)
+def thresholding(data, threshold):
+   transformed_image = data > threshold
+   #print(transformed_image)
+   transformed_fig = plt.figure(facecolor='black')
+   transformed_image_plot = transformed_fig.add_subplot(111)
+   plt.xticks([])
+   plt.yticks([])
+   time.sleep(0.0016)
+   transformed_image_plot.imshow(transformed_image, cmap='gray')
+   plt.savefig("temp/plot.png", format='png', dpi= 120 , bbox_inches='tight', pad_inches=0)
+   #print("threshold applied")
+   
+def isodata(image_data, threshold_0=200, tolerance= 0.001):
+   iteraciones = 0
+   threshold = threshold_0
+   print("data",image_data[0,1])
+
+   while True:
+
+      # umbralizacion
+      iterated_image = image_data > threshold
+
+      # hallamos el promedio
+      m_region_of_interest = image_data[iterated_image == 1].mean()
+      m_background = image_data[iterated_image == 0].mean()
+
+      new_threshold = 0.5 * (m_region_of_interest + m_background)
+
+      if abs(new_threshold - threshold) < tolerance:
+         break
+  
+      iteraciones = iteraciones + 1
+
+      threshold = new_threshold
+      #print("nuevo threshold",new_threshold)
+      
+   
+   # isodata image
+   isodata_fig = plt.figure(facecolor='black')
+   isodata_image_plot = isodata_fig.add_subplot(111)
+   plt.xticks([])
+   plt.yticks([])
+   time.sleep(0.0016)
+   isodata_image_plot.imshow(iterated_image, cmap='gray')
+   plt.savefig("temp/plot.png", format='png', dpi= 120 , bbox_inches='tight', pad_inches=0)
+   #print("Isodata applied")
+   return new_threshold
+   
+def laplacian(data,inksize,inscale,indelta):
+   data = cv2.Laplacian(data, -1, ksize=inksize, scale=inscale, delta=indelta, borderType=cv2.BORDER_DEFAULT)
+   #data = cv2.Laplacian(data, -1, ksize=5, scale=1,delta=0, borderType=cv2.BORDER_DEFAULT)
    plt.imshow(data, cmap="gray")
    plt.xticks([])
    plt.yticks([])
-   plt.savefig("output/plot.png", format='png', dpi= 120, bbox_inches='tight', pad_inches=0)
-   print("laplacian applied")
+   time.sleep(0.0016)
+   plt.savefig("temp/plot.png", format='png', dpi= 120, bbox_inches='tight', pad_inches=0)
+   #print("laplacian applied")
+
+# Region Growing
+
+
+# Distance Function to calculate how similar two pixels are
+def distance(x0,y0,x1,y1,Imagen_data):
+   print(Imagen_data.shape)
+   return abs(Imagen_data[x1,y1] - Imagen_data[x0,y0])
+
+# obtain the 4 neighbours of a pixel considering the size of the image
+def N4(x0,y0,M,N):
+   neighborhood = []
+   # right border
+   if x0+1 < M: neighborhood.append((x0+1,y0))
+   # left border
+   if x0-1 > 0: neighborhood.append((x0-1,y0))
+   # lower border
+   if y0+1 < N: neighborhood.append((x0,y0+1))
+   # upper border
+   if y0-1 > 0: neighborhood.append((x0,y0-1))
+   return neighborhood
+
+# obtain the neighbours of a pixel considering the size of the image
+def N8(x0,y0,M,N):
+   neighborhood = []
+   # right border
+   if x0+1 < M: neighborhood.append((x0+1,y0))
+   # left border
+   if x0-1 > 0: neighborhood.append((x0-1,y0))
+   # lower border
+   if y0+1 < N: neighborhood.append((x0,y0+1))
+   # upper border
+   if y0-1 > 0: neighborhood.append((x0,y0-1))
+   # right upper corner
+   if x0+1 < M and y0-1 > 0: neighborhood.append((x0+1,y0-1))
+   # left upper corner
+   if x0-1 > 0 and y0-1 > 0: neighborhood.append((x0-1,y0-1))
+   # right lower corner
+   if x0+1 < M and y0+1 < N: neighborhood.append((x0+1,y0+1))
+   # left lower corner
+   if x0-1 > 0 and y0+1 < N: neighborhood.append((x0-1,y0+1))
+   #print("neigborhood size ",len(neighborhood), neighborhood)
+   return neighborhood
+
+#def regionGrowing(selection_mask, image ,Tolerance , max_iter=1e6):
+   mask = np.zeros(image.shape)
+   visited = np.zeros(image.shape)
+   
+   print("mask",selection_mask)
+   visited_mask_image = Image.open("temp/visited_mask.png").convert("1")
+   visited = np.array(visited_mask_image)
+   #print("visited",visited)
+   original_image = Image.open("temp/plot.png")
+   Image_data = np.array(original_image)
+   print("original",Image_data[20,20])
+   
+   
+   # Get non-zero coordinates
+   nonzero_coords = []
+   for x in range(mask.shape[0]):
+      for y in range(mask.shape[1]):
+         if mask[x, y] != 0:
+               nonzero_coords.append((x, y))
+   # print("nonzero", nonzero_coords)
+               
+   # Get visited coordinates
+   visited_coords = []
+   for x in range(visited.shape[0]):
+      for y in range(visited.shape[1]):
+         if visited[x, y] != 0:
+               visited_coords.append((x, y))
+   #print("visited", visited_coords)
+               
+   #print("size",selection_mask.width, selection_mask.height)
+   # actual size of the image
+   N,M = image.shape
+   #print("m y n son",M,N)
+
+   # define a queue where the points to be procesed goes
+   neighbor_queue = nonzero_coords
+   
+   #print("neighbors",neighbor_queue )
+
+   iterations = 0
+
+   # This will run until the neighbor list is empty
+   while len(neighbor_queue) > 0:
+      # Get the first element of the list
+      x0, y0 = neighbor_queue.pop()
+
+      # Iterate over the neighbors
+      for x1, y1 in N4(x0, y0, M, N):
+         if 0 <= x1 < M and 0 <= y1 < N and abs(Image_data[x1, y1,0] - Image_data[x0, y0,0]) <= Tolerance and np.all(mask[x1, y1,0] == 0) and np.all(visited[x1, y1,0] == 0):
+            print('yes')
+            neighbor_queue.append((x1, y1))
+
+      mask[x0, y0,:] = 1
+      visited[x0, y0,:] = 1
+      iterations += 1
+
+      if iterations > max_iter:
+         print("overgrown")
+         break
+
+   growing_fig = plt.figure(facecolor='black')
+   growing_image_plot = growing_fig.add_subplot(111)
+   growing_image_plot.imshow(mask, cmap='gray')
+   plt.xticks([])
+   plt.yticks([])
+   time.sleep(0.0016)
+   plt.savefig("temp/plot.png", format='png', dpi=120, bbox_inches='tight', pad_inches=0)
+   
+   #return mask
+
+
+def regionGrowing(Image_data ,Tolerance, max_iter):
+   mask = np.zeros(Image_data.shape)
+   visited = np.zeros(Image_data.shape)
+   #print("mask size",mask.shape)
+   
+   mask_image = Image.open("temp/green_mask.png").convert('L')
+   mask_array = np.array(mask_image)
+   #print("dato?",mask_array)
+   # get coordenates where the pixel has a value above 0
+   true_coords = list(zip(*np.where(mask_array > 0)))
+   #print("las tuplas",true_coords)
+   
+   N,M = Image_data.shape
+   
+   neighbors_queue = []
+   neighbors_queue = true_coords
+   #neighbors_queue.append(true_coords)
+   #print("neigh queue", neighbors_queue)
+    
+   iterations = 0
+   
+   while neighbors_queue:
+      x0,y0 = neighbors_queue.pop()
+      print(x0,y0)
+      for x1,y1 in N4(x0,y0,M,N):
+         if(distance(x0,y0,x1,y1,Image_data)) <= Tolerance and mask[x1,y1] == 0 and visited[x1,y1] == 0:
+            
+            print("Dimensiones de Image_data:", Image_data.shape)
+            print("Coordenadas (x1, y1):", x1, y1)
+            neighbors_queue.append((x1,y1))
+      mask[x0,y0] = 1
+      visited[x0,y0] = 1
+      iterations += 1
+      
+      if iterations > max_iter:
+         print("overgrown")
+         break
+   
+   # GrownRegion image
+   grownRegion_fig = plt.figure(facecolor='black')
+   grownRegion_image_plot = grownRegion_fig.add_subplot(111)
+   plt.xticks([])
+   plt.yticks([])
+   time.sleep(0.0016)
+   grownRegion_image_plot.imshow(mask, cmap='gray')
+   plt.savefig("temp/plot.png", format='png', dpi= 120 , bbox_inches='tight', pad_inches=0)
+      
+   #return mask
--- GUI/main.py
+++ GUI/main.py
@@ -1,3 +1,4 @@
+import time
 import customtkinter as ctk
 import numpy as np
 import tkinter as tk
@@ -6,13 +7,13 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import filters
 from tkinter import Toplevel, filedialog
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw
 
 root = ctk.CTk()
 
 # Window dimensions and centering
 window_width = 800
-window_height = 900
+window_height = 800
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 x = (screen_width - window_width) // 2
@@ -20,32 +21,55 @@ y = (screen_height - window_height) // 2
 root.geometry(f"{window_width}x{window_height}+{x}+{y}")
 root.title("MRI Segmentation Tool")
 
+# to find the range of the threshold slider
+max_value = 0
 pen_color = ""
-pen_size = 15
+pen_size = 7
 gaussian_intensity = 1
 file_path = ""
 nii_2d_image = []
 nii_3d_image = []
 nii_3dimage_backup = []
 slice_portion = 100
+ksize_amount = 5
+scale_number = 1
+delta_factor = 0
+threshold_value = 100
+isodata_tolerance = 0.001
+isodata_threshold = 200
+tolerance_value=10
+
+
 # Radio buttons (0) Axial, (1) Coronal, (2) Sagittal
 view_mode = ctk.StringVar(value="Axial")
+selection_image = Image.new("RGB",(200,200),(255,255,255))
 
 def refresh_image():
+    global selection_image
     # placing plot in the canvas
-    plot_image = Image.open("output/plot.png")
+    time.sleep(0.0008)
+    plot_image = Image.open("temp/plot.png")
     #width, height = int(plot_image.width * 3), int(plot_image.height * 3) # upscaling the plot_image
     #plot_image = plot_image.resize((width, height))
     
     # adjusting the canvas to be the same size
     drawing_canvas.config(width=plot_image.width, height=plot_image.height)
+    picture_canvas.config(width=plot_image.width, height=plot_image.height)
+    selection_image = Image.new("RGB",(plot_image.width,plot_image.height),(0,0,0))
+    selection_image.save("temp/selection_canvas.png",format='png')
+    selection_image.save("temp/green_mask.png",format='png')
+    selection_image.save("temp/red_mask.png",format='png')
+    selection_image.save("temp/visited_mask.png",format='png')
+
     
     image = ImageTk.PhotoImage(plot_image)
-    drawing_canvas.image = image
-    drawing_canvas.create_image(0, 0, image=image, anchor="nw")
+    picture_canvas.image = image
+    picture_canvas.create_image(0, 0, image=image, anchor="nw")
+    #drawing_canvas.image = image
+    #drawing_canvas.create_image(0, 0, image=image, anchor="nw")
 
 def plot_image():
-    global nii_2d_image, nii_3d_image, pen_color, slice_portion
+    global max_value, nii_2d_image, nii_3d_image, pen_color, slice_portion
     
     #mpl.rcParams['savefig.pad_inches'] = 0
     
@@ -57,6 +81,9 @@ def plot_image():
         nii_2d_image = nii_3d_image[:,slice_portion,:]
     elif(view_mode.get() == "Sagittal"):
         nii_2d_image = nii_3d_image[slice_portion,:,:]
+        
+    # to find the range of the threshold slider
+    max_value = nii_2d_image[nii_2d_image > 0].flatten().max()
     
     # plotting data
     plt.axes(frameon=False)
@@ -67,16 +94,20 @@ def plot_image():
     plt.xticks([])
     plt.yticks([])
     plt.autoscale(tight=True)
-    plt.savefig("output/plot.png", format='png', dpi= 120, bbox_inches='tight', pad_inches=0)
+    time.sleep(0.0008)
+    plt.savefig("temp/plot.png", format='png', dpi= 120, bbox_inches='tight', pad_inches=0)
+    plt.savefig("temp/original.png", format='png', dpi= 120, bbox_inches='tight', pad_inches=0)
     
     refresh_image()
     
     mode_switch.configure(state="normal")
     pen_size_scale.configure(state="normal")
     clear_button.configure(state="normal")
-    process_segmentation_button.configure(state="normal")
+    filters_button.configure(state="normal")
     slice_slider.configure(state="normal")
-    pen_color="green3"
+    segmentation_button.configure(state="normal")
+    Tolerance_slider.configure(state="normal")
+    pen_color="#00cd00"
 
 def add_image():
     global file_path, pen_color, nii_2d_image, nii_3dimage_backup, nii_3d_image
@@ -114,6 +145,44 @@ def change_gaussian_val(val):
     filters.gaussian(nii_2d_image,gaussian_intensity)
     refresh_image()
     
+def change_ksize_val(val):
+    global ksize_amount
+    ksize_amount = int(val)
+    filters.laplacian(nii_2d_image,ksize_amount,scale_number, delta_factor)
+    refresh_image()
+    
+def change_scale_val(val):
+    global scale_number
+    scale_number = int(val)
+    filters.laplacian(nii_2d_image,ksize_amount,scale_number, delta_factor)
+    refresh_image()
+    
+def change_delta_val(val):
+    global delta_factor
+    delta_factor = int(val)
+    filters.laplacian(nii_2d_image,ksize_amount,scale_number, delta_factor)
+    refresh_image()
+    
+def change_threshold_val(val):
+    global threshold_value
+    threshold_value = int(val)
+    filters.thresholding(nii_2d_image,threshold_value)
+    refresh_image()
+    
+def change_isodata_threshold_val(val):
+    global isodata_threshold
+    isodata_threshold = int(val)
+    
+def change_isodata_tolerance_val(val):
+    global isodata_tolerance
+    isodata_tolerance = int(val)
+    
+def change_tolerance_val(val):
+    global tolerance_value
+    tolerance_value = int(val)
+    Tolerance_slider.set(val)
+
+    
 def switch_event():
     if(switch_var.get() == "on"):
         include()
@@ -122,16 +191,31 @@ def switch_event():
 
 def include():
     global pen_color
-    pen_color = "green3"
+    pen_color = "#00cd00"
     
 def exclude():
     global pen_color
-    pen_color = "red"
+    pen_color = "#FF0000"
 
 def draw(event):
     global last_x, last_y
     x, y = event.x, event.y
+    #drawing_canvas.create_line(last_x, last_y, x, y, fill=pen_color, width=pen_size*2, capstyle="round", smooth=True)
+    picture_canvas.create_line(last_x, last_y, x, y, fill=pen_color, width=pen_size*2, capstyle="round", smooth=True)
+    
+    # Save canvas image
+    draw_selection = Image.open("temp/selection_canvas.png")
+    
+    # Draw on the canvas image
+    draw = ImageDraw.Draw(draw_selection)
+    draw.line((last_x, last_y, x, y), fill=pen_color, width=pen_size*2)
     drawing_canvas.create_line(last_x, last_y, x, y, fill=pen_color, width=pen_size*2, capstyle="round", smooth=True)
+    draw_selection.save("temp/selection_canvas.png", format='png')
+    
+    #draw = ImageDraw.Draw(draw_selection)
+    #draw.line((last_x, last_y, x, y), fill=pen_color, width=pen_size*2, joint='curve')
+    #draw_selection.save("temp/selection_canvas.png",format='png')
+    
     last_x, last_y = x, y
 
 def start_draw(event):
@@ -139,20 +223,50 @@ def start_draw(event):
     last_x, last_y = event.x, event.y
 
 def end_draw(event):
+    #referencia.save()
     pass
  
 def clear_canvas():
     global pen_size, pen_color
     drawing_canvas.delete("all")
-    drawing_canvas.create_image(0, 0, image=drawing_canvas.image, anchor="nw")
-    pen_size = 15
-    pen_size_scale.set(15)
-    pen_color = "green3"
-    mode_switch.select()     
+    picture_canvas.create_image(0, 0, image=picture_canvas.image, anchor="nw")
+    plot_image = Image.open("temp/plot.png")
+    plot_original = Image.open("temp/original.png")
+    plot_original.save("temp/plot.png", format='png')
+    selection_image = Image.new("RGB",(plot_image.width,plot_image.height),(0,0,0))
+    selection_image.save("temp/selection_canvas.png",format='png')
+    selection_image.save("temp/green_mask.png",format='png')
+    selection_image.save("temp/red_mask.png",format='png')
+    pen_size_scale.set(7)
+    pen_color = "#00cd00"
+    mode_switch.select()
+    refresh_image()     
+
 
 def filters_window():
     
     global nii_2d_image
+    
+    def restore_sliders():
+        global threshold_value,gaussian_intensity,slice_portion,ksize_amount, scale_number, delta_factor
+        gaussian_intensity=0
+        gaussian_slider.set(0)
+        ksize_amount=5
+        ksize_slider.set(5)
+        scale_number=1
+        scale_slider.set(1)
+        delta_factor=0
+        delta_slider.set(0)
+        threshold_value=100
+        threshold_slider.set(100)
+        isodata_threshold_slider.set(0)
+        
+    def apply_isodata():
+        new_threshold = filters.isodata(nii_2d_image,isodata_threshold,isodata_tolerance)
+        change_isodata_threshold_val(new_threshold)
+        isodata_threshold_slider.set(new_threshold)
+        refresh_image()
+
     
     # Toplevel object which will 
     # be treated as a new window
@@ -161,14 +275,14 @@ def filters_window():
     options = []
     
     # deactivate the filters button while this windows is open to avoid repeated instances
-    process_segmentation_button.configure(state="disabled")
+    filters_button.configure(state="disabled")
     
     # sets the title of the
     # Toplevel widget
     filters_window.title("Image Filters Selector")
  
     # sets the geometry of toplevel
-    filters_window.geometry("400x600")
+    filters_window.geometry("900x450")
 
     # spacer
     ctk.CTkLabel(master=filters_window,text="Filtering Options", height=40).pack(pady=15)
@@ -204,25 +318,124 @@ def filters_window():
     # Laplacian Label
     laplacian_label = ctk.CTkLabel(master=laplacian_frame, text="Laplacian Options", height=10)
     laplacian_label.pack(pady=15)
+    
+    # Label for the ksize slider
+    label_ksize = ctk.CTkLabel(master=laplacian_frame, text="Laplacian Kernel Size")
+    label_ksize.pack()
 
-    # Laplacian filter button (edge detection)
-    laplacian_filter_button = ctk.CTkButton(master=laplacian_frame, text="Laplacian edges", command= lambda: [filters.laplacian(nii_2d_image),refresh_image()])
-    laplacian_filter_button.pack(pady=5)
+    # ksize slider
+    ksize_slider = ctk.CTkSlider(master=laplacian_frame, from_=0, to=13, command=change_ksize_val, width=120)
+    ksize_slider.set(5)
+    ksize_slider.pack(pady=5)
+    
+    # Label for the scale slider
+    label_scale = ctk.CTkLabel(master=laplacian_frame, text="Laplacian Scale")
+    label_scale.pack()
+
+    # scale slider
+    scale_slider = ctk.CTkSlider(master=laplacian_frame, from_=1, to=10, command=change_scale_val, width=120)
+    scale_slider.set(1)
+    scale_slider.pack(pady=5)
+    
+    # Label for the scale slider
+    label_delta = ctk.CTkLabel(master=laplacian_frame, text="Laplacian Delta")
+    label_delta.pack()
+
+    # scale slider
+    delta_slider = ctk.CTkSlider(master=laplacian_frame, from_=1, to=10, command=change_delta_val, width=120)
+    delta_slider.set(1)
+    delta_slider.pack(pady=5)
+    
+    # Thresholding frame
+    thresholding_frame = tk.Frame(master=filters_frame)
+    thresholding_frame.grid(row=0, column=2, padx=15, pady=5)
+    
+    # Thresholding options
+    gaussian_label = ctk.CTkLabel(master=thresholding_frame, text="Thresholding Options", height=10)
+    gaussian_label.pack(pady=15)
+
+    # Label for the Threshold slider
+    label_Threshold = ctk.CTkLabel(master=thresholding_frame, text="Threshold:")
+    label_Threshold.pack()
+
+    # Threshold  slider
+    threshold_slider = ctk.CTkSlider(master=thresholding_frame, from_=0, to=max_value, command=change_threshold_val, width=120)
+    threshold_slider.set(100)
+    threshold_slider.pack(pady=5)
     
     buttons_frame = tk.Frame(master=filters_window)
     buttons_frame.pack(pady=30)
     
-    # An apply button to confirm changes
-    apply_button = ctk.CTkButton(master=buttons_frame, text ="Apply", command=lambda: [filters_window.destroy()])
-    apply_button.grid(row=0, column=0, padx=5, pady=5)
+    
+    # Isodata frame
+    isodata_frame = tk.Frame(master=filters_frame)
+    isodata_frame.grid(row=0, column=3, padx=15, pady=5)
+    #gaussian_frame.pack()
+    
+    # Gaussian slider
+    isodata_label = ctk.CTkLabel(master=isodata_frame, text="Isodata Options", height=10)
+    isodata_label.pack(pady=15)
+
+    # Label for the Gaussian slider
+    label_Isodata = ctk.CTkLabel(master=isodata_frame, text="Isodata initial threshold:")
+    label_Isodata.pack()
+
+    # Isodata threshold slider
+    isodata_threshold_slider = ctk.CTkSlider(master=isodata_frame, from_=0, to=max_value, command=change_isodata_threshold_val, width=120)
+    isodata_threshold_slider.set(100)
+    isodata_threshold_slider.pack(pady=5)
+    
+    # An apply button for isodata iterations
+    isodata_apply_button = ctk.CTkButton(master=isodata_frame, text ="Apply Isodata", command=apply_isodata)
+    isodata_apply_button.pack(pady=5)
     
     # restore image Button
-    restore_image_button = ctk.CTkButton(master=buttons_frame, text="Restore Image",  command=lambda: [restore_data(),gaussian_slider.set(0)])
+    restore_image_button = ctk.CTkButton(master=buttons_frame, text="Restore Image",  command=lambda: [restore_data(),restore_sliders()])
     restore_image_button.grid(row=0, column=0, padx=5, pady=5)
     
     # A cancel button to discard changes
-    close_button = ctk.CTkButton(master=buttons_frame, text ="Close", command=lambda: [filters_window.destroy(),process_segmentation_button.configure(state="normal")])
+    close_button = ctk.CTkButton(master=buttons_frame, text ="Close", command=lambda: [filters_window.destroy(),filters_button.configure(state="normal")])
     close_button.grid(row=0, column=1, padx=5, pady=5)
+    
+def apply_segmentation():
+    selection_image = Image.open("temp/selection_canvas.png")
+    selection_array = np.array(selection_image)
+    
+    red_mask = (selection_array[:,:,0] > 200)
+    red_mask_fig = plt.figure(facecolor='black')
+    red_mask_plot = red_mask_fig.add_subplot(111)
+    red_mask_image = plt.imshow(red_mask, cmap='gray')
+    plt.xticks([])
+    plt.yticks([])
+    plt.savefig("temp/red_mask.png", format='png', dpi= 120 , bbox_inches='tight', pad_inches=0)
+    time.sleep(0.0016)
+    
+    green_mask = (selection_array[:,:,1] > 200)
+    #print("green mask", green_mask[1,1])
+    green_mask_fig = plt.figure(facecolor='black')
+    green_mask_plot = green_mask_fig.add_subplot(111)
+    green_mask_plot.imshow(green_mask, cmap='gray')
+    plt.xticks([])
+    plt.yticks([])
+    time.sleep(0.0016)
+    plt.savefig("temp/green_mask.png", format='png', dpi= 120 , bbox_inches='tight', pad_inches=0)
+    
+    visited_mask = np.logical_or(selection_array[:,:,1] > 200, selection_array[:,:,0] > 200)
+    visited_mask_fig = plt.figure(facecolor='black')
+    visited_mask_plot = visited_mask_fig.add_subplot(111)
+    visited_mask_plot.imshow(visited_mask, cmap='gray')
+    plt.xticks([])
+    plt.yticks([])
+    time.sleep(0.0016)
+    plt.savefig("temp/visited_mask.png", format='png', dpi= 120 , bbox_inches='tight', pad_inches=0)
+    #print("Mask created")
+    
+    original_image = Image.open("temp/original.png").convert('L')
+    original_image_plot = np.array(original_image)
+    
+    #print("shapeshape",original_image_plot.shape)
+    filters.regionGrowing(original_image_plot ,tolerance_value,10000)
+    refresh_image()
     
 def restore_data():
     global nii_2d_image, nii_3dimage_backup
@@ -257,14 +470,22 @@ def update_scroll_region(event):
 # Bind event to update scroll region
 tools_frame.bind('<Configure>', update_scroll_region)
 
-# Canvas Area on the rest of the window
+# diferent Canvas overlaped on the rest of the window
+# the main canvas frame
 canvas_frame = ctk.CTkFrame(root, width=750, height=600)
 canvas_frame.pack(pady=50, padx=15)
-drawing_canvas = ctk.CTkCanvas(canvas_frame, width=750, height=600)
-drawing_canvas.pack()
+
+
+# the picture canvas where we show the image (under the drawing canvas)
+picture_canvas = ctk.CTkCanvas(canvas_frame, width=750, height=600)
+picture_canvas.pack()
+
+# the drawing canvas where we keep the user selection
+drawing_canvas = tk.Canvas(canvas_frame, width=750, height=600)
+#drawing_canvas.grid(column=1,row=0)
 
 # Logo
-logo_frame = ctk.CTkFrame(master=left_frame_canvas)
+logo_frame = ctk.CTkFrame(master=left_frame_canvas,bg_color="transparent")
 logo_frame.grid(row=0, pady=15, padx=30)
 canvas_logo = tk.Canvas(master=logo_frame, width=100, height=100)
 canvas_logo.grid(row=0)
@@ -273,11 +494,11 @@ canvas_logo.create_image(50, 50, image=logo_image)
 
 # Title of the tool under the logo
 title_label = ctk.CTkLabel(logo_frame, text="MRI Slice \n Segmentation Tool \n (beta)")
-title_label.grid(row=1)
+title_label.grid(row=1, pady=15)
 
 # Upload Button
 upload_button = ctk.CTkButton(left_frame_canvas, text='Upload Image', command=add_image)
-upload_button.grid(row=2,pady=15, padx=20)
+upload_button.grid(row=2,pady=10, padx=20)
 
 # Frame for radio buttons
 radio_frame = ctk.CTkFrame(master=left_frame_canvas)
@@ -303,7 +524,7 @@ slice_slider.set(slice_portion)
 slice_slider.grid(row=5)
 
 # margin
-ctk.CTkFrame(master=left_frame_canvas, height=20).grid(row=6,pady=5)
+ctk.CTkFrame(master=left_frame_canvas, height=20,bg_color="transparent").grid(row=6,pady=5)
 
 # switch for include/eclude pen
 label = ctk.CTkLabel(master=left_frame_canvas, text="Pen Mode:")
@@ -324,7 +545,7 @@ label = ctk.CTkLabel(master=left_frame_canvas, text="Pen Size:")
 label.grid(row=9,pady=5)
 
 # slider
-pen_size_scale = ctk.CTkSlider(master=left_frame_canvas, from_=5, to=25,state="disabled", command=change_pen_size, width=120)
+pen_size_scale = ctk.CTkSlider(master=left_frame_canvas, from_=1, to=13,state="disabled", command=change_pen_size, width=120)
 pen_size_scale.set(pen_size)
 pen_size_scale.grid(row=10)
 
@@ -334,12 +555,34 @@ ctk.CTkFrame(master=left_frame_canvas, height=20).grid(row=11,pady=5)
 clear_button = ctk.CTkButton(left_frame_canvas, text="Clear Selection", state="disabled", command=clear_canvas)
 clear_button.grid(row=12,pady=10)
 
-# Process image button
-process_segmentation_button = ctk.CTkButton(left_frame_canvas, text="Filters", state="disabled", command=filters_window)
-process_segmentation_button.grid(row=13,pady=10, padx=20)
+# Frame for segmentation tools
+segmentation_tools_frame = ctk.CTkFrame(master=left_frame_canvas)
+segmentation_tools_frame.grid(row=13,pady=10,padx=20)
 
-drawing_canvas.bind("<Button-1>", start_draw)
-drawing_canvas.bind("<B1-Motion>", draw)
-drawing_canvas.bind("<ButtonRelease-1>", end_draw)
+# Label for the tolerance slider
+label_tolerance = ctk.CTkLabel(master=segmentation_tools_frame, text="Tolerance:")
+label_tolerance.pack(pady=5)
+
+# slider tolerance
+Tolerance_slider = ctk.CTkSlider(master=segmentation_tools_frame, from_=1, to=192,state="disabled", command=change_tolerance_val, width=120)
+Tolerance_slider.set(25)
+Tolerance_slider.pack(pady=5)
+
+# Process image segmentation button
+segmentation_button = ctk.CTkButton(segmentation_tools_frame, text="Apply Region Growing", state="disabled", command=apply_segmentation)
+segmentation_button.pack(pady=5)
+
+# Filters button
+filters_button = ctk.CTkButton(left_frame_canvas, text="Filters", state="disabled", command=filters_window)
+filters_button.grid(row=14,pady=10, padx=20)
+
+picture_canvas.bind("<Button-1>", start_draw)
+picture_canvas.bind("<B1-Motion>", draw)
+picture_canvas.bind("<ButtonRelease-1>", end_draw)
+
 
 root.mainloop()
+
+
+#mymask = growing(80,84,image_data,25.3,,1000000)
+#regionGrowing(x0,y0,Image,Tolerance,max_iter):
